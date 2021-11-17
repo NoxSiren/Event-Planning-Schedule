e833521b891a28ea3f7a96c3960d31f59abc4eba
Jaki Hawker <jmichel910@yahoo.com>
2021-11-16 18:34:21 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+//Create daily planner schedule
+//When planner is open, it opens to the current date at the top
+//When scrolling down, 9am-5pm blocks
+//When timeblocks are seen, it is color coded for events past, present, and future
+//You can click on a time block, enter an event, and click a save button to save the event
+//Text will be saved in local storage
+//Refresh the page and saved events will be prelisted
+
